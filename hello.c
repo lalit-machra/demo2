3f8e1f8c00bceff04e36c16636638b593b2c87ea
Lalit Machra <lalitmachra0403@gmail.com>
2023-07-21 17:57:12 +0530

    Project 1

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Lalit this side\n");
+    return 0;
+}
---
+++ hii.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    printf("Hello, my name is lalit\n");
+    return 0;
+}
