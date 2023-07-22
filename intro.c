871b386a8c30c53afca00858c6b4b79d55714552
Lalit Machra <lalitmachra0403@gmail.com>
2023-07-22 09:23:20 +0530

    tracked intro.c again

---
+++ intro.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("this is a new file\n");
+    
+    for (int i = 1; i <= 5; i++)
+    {
+        printf("%i\t");
+    }
+    printf("\n");
+
+    return 0;
+}
